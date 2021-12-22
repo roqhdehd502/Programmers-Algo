9adb1170214b110bb7294b8c573586618b144d2e
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2021-12-22 16:26:08 +0900

    Update RowAndHighRankForLotto.java

--- 2021 Dev-Matching Web Back-end Dev/RowAndHighRankForLotto.java
+++ 2021 Dev-Matching Web Back-end Dev/RowAndHighRankForLotto.java
@@ -1,6 +1,30 @@
+/**
+로또의 최고 순위와 최저 순위
+*/
+
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = {};
+        int rnk = 7;
+        int cnt = 0;
+        int[] answer = {0, 0};
+        
+        for(int i=0; i<lottos.length; i++) { // 비교해야할 인덱스의 가짓 수가 36이므로 브루트포스 탐색을 진행한다
+            for(int j=0; j<win_nums.length; j++) {
+                if(lottos[i] == win_nums[j]) { rnk--; } // 두 인덱스가 서로 같으면 순위 증가
+            }
+            
+            if(lottos[i] == 0) { cnt++; } // lottos의 인덱스가 0이면 카운트 증가
+        }
+        
+        answer[0] = rnk - cnt; // 최고 순위
+        if(rnk == 7) { // 최저 순위
+            answer[1] = 6;
+        } else {
+            answer[1] = rnk; 
+        }
+        
         return answer;
     }
 }
